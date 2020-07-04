e334d0381d90330a0e177ae155052e970abe284a
Jonathan Silva Viana <jonathantepes@hotmail.com>
2020-07-04 20:10:12 -0300

    home design completo

--- app/src/main/java/br/com/ichickenyou/MainActivity.java
+++ app/src/main/java/br/com/ichickenyou/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.ichickenyou;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +12,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
     }
 
 }
