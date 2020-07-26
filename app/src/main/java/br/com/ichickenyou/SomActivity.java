a8359e3b517a5ecd9375d984f3b4ced36d81840a
Jonathan Silva Viana <jonathantepes@hotmail.com>
2020-07-26 11:59:44 -0300

    layout audio completo

--- app/src/main/java/br/com/ichickenyou/SomActivity.java
+++ app/src/main/java/br/com/ichickenyou/SomActivity.java
@@ -10,5 +10,8 @@ public class SomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_som);
+
+        //define título da activity
+        setTitle(R.string.sobre_som);
     }
 }
