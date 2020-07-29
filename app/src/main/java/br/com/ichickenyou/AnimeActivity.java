1320e0e397c6c381a4c00e3e2a35edef8660ed47
Jonathan Silva Viana <jonathantepes@hotmail.com>
2020-07-29 10:50:27 -0300

    anime activity completa

--- app/src/main/java/br/com/ichickenyou/AnimeActivity.java
+++ app/src/main/java/br/com/ichickenyou/AnimeActivity.java
@@ -1,6 +1,7 @@
 package br.com.ichickenyou;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class AnimeActivity extends AppCompatActivity {
 
@@ -43,6 +46,9 @@ public class AnimeActivity extends AppCompatActivity {
                 //encerra o tempo de exibição
                 SPLASH_TIME_OUT);
 
+        //chama o método que aplica a denifição de idioma sobre fontes
+        define_idioma();
+
     }
 
     public void comSom()
@@ -70,4 +76,30 @@ public class AnimeActivity extends AppCompatActivity {
             Log.d("audio desativado", "audio desativado para tocar o som de play");
         }
     }
+
+    public void define_idioma()
+    {
+        //variável que define a string do idioma coreano
+        idioma_coreano = "ko";
+
+        //variável boolean que define se o idioma rastreado, nesse caso encapsulado da variável "idioma_coreano", acima;
+        se_idioma_coreano = Locale.getDefault().getLanguage().equals(idioma_coreano);
+
+        //essa variável coleta a string do idioma local que esteja no aparelho que execute a aplicação
+        outros_idiomas = Locale.getDefault().getLanguage();
+
+        //executa a condicional se a idioma for Coreano, muda a fonte no layout difinido no método oncreate, para uma fonte compatível com o idioma coreano.
+
+        if (se_idioma_coreano == true) {
+            mainTextView = (TextView)findViewById(R.id.apresentcao_texto);
+            typeface = ResourcesCompat.getFont(this, R.font.koreanfont);
+            mainTextView.setTypeface(typeface);
+            mainTextView.setTextSize(43);
+        }
+        else {
+            //exibe um log do sistema dizendo qual o idioma rastreado
+            Log.d("IDIOMA:", outros_idiomas);
+        }
+}
+
 }
