bad69c5d5d567b6ca2efc563487f808145a8924e
Jonathan Silva Viana <jonathantepes@hotmail.com>
2020-09-27 17:12:54 -0300

    activities de gênero incluídas

--- app/src/main/java/br/com/ichickenyou/GenderActivity.java
+++ app/src/main/java/br/com/ichickenyou/GenderActivity.java
@@ -11,7 +11,7 @@ public class GenderActivity extends AppCompatActivity {
 
     ImageButton btMale, btFemale, btDoubtorPreferDontSay, manyoranothergenders;
     Intent envia_homem, envia_mulher, envia_btDoubtorPreferDontSay, envia_manyoranothergenders;
-    String man, woman, anyGender;
+    String man, woman, anyGender, gender, impossible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,12 @@ public class GenderActivity extends AppCompatActivity {
 
         //define título da activity
         setTitle(R.string.app_name);
+
+        //define strings
+
+        man = "Homem"; woman = "Mulher"; anyGender = "qualquergenero"; impossible = "impossivel";
+
+        //encontra os botões e abaixo redireciona como um router para as opções
 
         btMale = (ImageButton)findViewById(R.id.btMale);
 
@@ -30,28 +36,53 @@ public class GenderActivity extends AppCompatActivity {
             }
         });
 
+        btFemale = (ImageButton)findViewById(R.id.btFemale);
+
+        btFemale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                envia_mulher();
+            }
+        });
+
+        btDoubtorPreferDontSay = (ImageButton)findViewById(R.id.btDoubtorPreferDontSay);
+
+        btDoubtorPreferDontSay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                envia_manyoranothergenders();
+            }
+        });
+
+        manyoranothergenders = (ImageButton)findViewById(R.id.manyoranothergenders);
+
+        manyoranothergenders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                envia_manyoranothergenders();
+            }
+        });
+
     }
 
+    //envia para homem
     public void envia_homem()
     {
-        envia_homem = new Intent(this, QuizGenderActivity.class);
-        man = "Homem";
-        envia_homem.putExtra(man, man);
+        envia_homem = new Intent(this, MaleQuizActivity.class);
         startActivity(envia_homem);
     }
 
+    //envia para mulher
     public void envia_mulher()
     {
-
+        envia_mulher = new Intent(this, FemaleQuizActivity.class);
+        startActivity(envia_mulher);
     }
 
-    public void envia_btDoubtorPreferDontSay()
-    {
-
-    }
-
+    //envia para qualquer gênero
     public void envia_manyoranothergenders()
     {
-
+        envia_manyoranothergenders = new Intent(this, AnyGenderQuizActivity.class);
+        startActivity(envia_manyoranothergenders);
     }
 }
